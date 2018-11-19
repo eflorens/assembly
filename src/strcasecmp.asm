7f9fc0548438a5c8f1da9d1c7f8a1b5180668023
eflorens <44293900+eflorens@users.noreply.github.com>
2018-11-19 12:24:27 +0100

    Update strcasecmp.asm

--- src/strcasecmp.asm
+++ src/strcasecmp.asm
@@ -1,45 +1,45 @@
-	bits 64	
+	BITS 64	
 
-	section .text
-	global	strcasecmp
+	SECTION .text
+	GLOBAL	strcasecmp
 
 strcasecmp:
-	xor	rcx, rcx
-	jmp	strcasecmp_loop_s1
+	XOR	RCX, RCX
+	JMP	strcasecmp_loop_s1
 
 uppercase_r8b:
-	sub	r8b, 32
-	jmp	strcasecmp_loop_s2
+	SUB	R8B, 32
+	JMP	strcasecmp_loop_s2
 
 uppercase_r9b:
-	sub	r9b, 32
-	jmp	strcasecmp_check
+	SUB	R9B, 32
+	JMP	strcasecmp_check
 	
 strcasecmp_loop_s1:
-	mov	r8b, [rdi + rcx]
-	mov	r9b, [rsi + rcx]
-	cmp	r8b, 97
-	jl	strcasecmp_loop_s2
-	cmp	r8b, 122
-	jle	uppercase_r8b
+	MOV	R8B, [RDI + RCX]
+	MOV	R9B, [RSI + RCX]
+	CMP	R8B, 97
+	JL	strcasecmp_loop_s2
+	CMP	R8B, 122
+	JLE	uppercase_r8b
 
 strcasecmp_loop_s2:
-	cmp	r9b, 97
-	jl	strcasecmp_check
-	cmp	r9b, 122
-	jle	uppercase_r9b
+	CMP	R9B, 97
+	JL	strcasecmp_check
+	CMP	R9B, 122
+	JLE	uppercase_r9b
 
 strcasecmp_check:
-	cmp	r8b, r9b
-	jne	strcasecmp_return
-	cmp	r8b, 0
-	je	strcasecmp_return
-	cmp	r9b, 0
-	je	strcasecmp_return
-	inc	rcx
-	jmp	strcasecmp_loop_s1
+	CMP	R8B, R9B
+	JNE	strcasecmp_return
+	CMP	R8B, 0
+	JE	strcasecmp_return
+	CMP	R9B, 0
+	JE	strcasecmp_return
+	INC	RCX
+	JMP	strcasecmp_loop_s1
 
 strcasecmp_return:
-	sub	r8b, r9b
-	movsx	rax, r8b
-	ret
+	SUB	R8B, R9B
+	MOVSX	RAX, R8B
+	RET
